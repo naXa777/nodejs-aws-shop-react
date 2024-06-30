695e37dd2c91e018f9b7d009a564b64bbe9149e5
Pavel <naxa@nextmail.ru>
2024-06-30 20:06:12 +0200

    feat: Migrate to new Import API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://waexur8syl.execute-api.eu-central-1.amazonaws.com/prod/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://oru6e8x93k.execute-api.eu-central-1.amazonaws.com/prod/import",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };

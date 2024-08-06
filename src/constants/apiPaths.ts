84716e50117a9c60ed0c6b080166d05ca8fae585
Pavel <naxa@nextmail.ru>
2024-08-07 01:40:16 +0200

    feat: set Cart API URL

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://oru6e8x93k.execute-api.eu-central-1.amazonaws.com/prod/import",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://naxa777-cart-api-demo.eu-central-1.elasticbeanstalk.com",
 };
 
 export default API_PATHS;
